7f7d6a8096626a3db00ec5834d6abac8719fbb95
Preston Culbertson <pculbert@caltech.edu>
2023-05-31 13:08:38 -0700

    changed tabs (shudder) to spaces, updated roadmap

--- jaxosqp/utils.py
+++ jaxosqp/utils.py
@@ -1,10 +1,10 @@
 import jax.numpy as jnp
 
 def vcat(*args):
-	return jnp.concatenate(args, axis=0)
+    return jnp.concatenate(args, axis=0)
 
 def hcat(*args):
-	return jnp.concatenate(args, axis=1)
+    return jnp.concatenate(args, axis=1)
 
 def linf_norm(x, **args):
-	return jnp.linalg.norm(x, ord=jnp.inf, **args)
+    return jnp.linalg.norm(x, ord=jnp.inf, **args)
